b825dbaf308dde8609d911faf5dc6c4f58f0ff8e
Nico Rehwaldt <git_nikku@nixis.de>
2014-08-08 17:04:45 +0200

    fix(debug-overlay): properly handle element ids in API

--- test/spec/debugger/debugger.js
+++ test/spec/debugger/debugger.js
@@ -1,12 +1,16 @@
+var _ = require('lodash');
+
 
 /**
  * A test debugger that does nothing.
  *
  * @param {EventBus} eventBus
  */
-function Debugger(eventBus) {
+function Debugger(eventBus, elementRegistry) {
 
   this._eventBus = eventBus;
+  this._elementRegistry = elementRegistry;
+
 
   eventBus.on('debug.breakpoint.remove', function(e) {
     eventBus.fire('debug.breakpoint.removed', e);
@@ -16,16 +20,41 @@ function Debugger(eventBus) {
     eventBus.fire('debug.breakpoint.added', e);
   });
 
+  var path = {
+    'StartEvent_1': 'ServiceTask_1',
+    'ServiceTask_1': 'ServiceTask_2',
+    'ServiceTask_2': 'ExclusiveGateway_1',
+    'ExclusiveGateway_1': 'ServiceTask_3',
+    'ServiceTask_3': 'ExclusiveGateway_2',
+    'ExclusiveGateway_2': 'EndEvent_1'
+  };
+
+  var self = this;
+
   eventBus.on('debug.play', function(e) {
-    console.log('debug.play', e);
+
+    var element = e.element;
+
+    var next = path[element.id];
+    if (!next) {
+      return;
+    }
+
+    next = self._elementRegistry.getById(next);
+
+    self.step(next);
   });
 }
 
 Debugger.prototype.step = function(shape) {
+  if (_.isString(shape)) {
+    shape = this._elementRegistry.getById(shape);
+  }
+
   this._eventBus.fire('debug.step', { element: shape });
 };
 
 
-Debugger.$inject = [ 'eventBus' ];
+Debugger.$inject = [ 'eventBus', 'elementRegistry' ];
 
 module.exports = Debugger;
