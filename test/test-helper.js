023fbceea1a9d14ea4aef8a78e8a453703817572
Nico Rehwaldt <git_nikku@nixis.de>
2015-03-17 18:17:38 +0100

    WIP(break): break after activity

--- test/test-helper.js
+++ test/test-helper.js
@@ -6,5 +6,5 @@ TestHelper.insertCSS('diagram-js.css', fs.readFileSync(__dirname + '/../node_mod
 TestHelper.insertCSS('debug-overlay.css', fs.readFileSync(__dirname + '/../assets/debug-overlay.css', 'utf8'));
 
 TestHelper.insertCSS('diagram-js-testing.css',
-  '.test-container .result { height: 500px; }' + '.test-container > div'
+  '.test-container { height: 500px; } '
 );
